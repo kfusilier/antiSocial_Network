df2a67236b975d2b07983bd56fb54a7e2b4dea16
kfusilier <kristinfusilier@gmail.com>
2022-02-28 18:36:55 -0700

    yarn dev working

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// bring in React from React
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// define our Hello functional component
+function Hello() {
+	// what should the component return
+	return (
+		// Make sure to return some UI
+		<h1>Hello World!</h1>
+	);
 }
 
-export default App;
+export default Hello;
+// export default App;
---
+++ packages/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	PORT: 4000,
+};
+
+// module.exports = {
+// 	PORT: process.env.PORT || 4000,
+// 	MONGO_URL: process.env.MONGO_URL,
+// };
