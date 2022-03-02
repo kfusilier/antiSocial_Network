f90999910481eadc796982f3aa1182b7301652b5
petesert <petesertthin@gmail.com>
2022-03-01 20:37:46 -0800

    User index controller function

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,6 +2,15 @@ const req = require("express/lib/request");
 const db = require("../models");
 const User = require("../models/User");
 
+const index = (req, res) => {
+    db.User.find({}, (err, foundUsers) => {
+        if (err) return res.send(err)
+        return res.render("users/index", {
+            users: foundUsers,
+        })
+    })
+}
+
 const show = (req, res) => {
     db.User.findById(req.params.id)
         .populate("posts")
