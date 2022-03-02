874c10f51c67ead37a676f2a45459b9da4798e7d
petesert <petesertthin@gmail.com>
2022-03-01 20:34:05 -0800

    set up show and newUser controller

---
+++ server/controllers/users.js
@@ -0,0 +1,26 @@
+const req = require("express/lib/request");
+const db = require("../models");
+const User = require("../models/User");
+
+const show = (req, res) => {
+    db.User.findById(req.params.id)
+        .populate("posts")
+        .populate("comments")
+        .exec((err, foundUser) => {
+            if (err) return res.send(err)
+            return res.render("users/show", {
+                user : foundUser,
+                loginUser : req.user
+            })
+        })
+}
+
+const newUser = (req, res) => {
+    res.render("users/new");
+}
+
+
+module.exports = {
+    newUser,
+    show,
+}
