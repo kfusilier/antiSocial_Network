0c915b740e4becb83174ebce226ebffc99b10833
X <xac923@outlook.com>
2022-03-04 16:17:40 -0800

    rendering all users in user list page

--- client/src/components/UserList/index.jsx
+++ client/src/components/UserList/index.jsx
@@ -1,8 +1,30 @@
-
+import { useEffect, useState } from "react";
+import antiSocialAppTo from "../../api/axios.config";
 
 const UserList = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        await antiSocialAppTo.get("/users").then((res) => {
+            setUsers(res.data.data);
+        });
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
     return(
-        <h1>UserList</h1>
+        <div>
+            {users.map((user) => {
+                return (
+                    <div>
+                <h2>{user.screenName}</h2>
+                <h3>{user.userName}</h3>
+                </div>
+            )})}
+            </div>
     )
 }
 
