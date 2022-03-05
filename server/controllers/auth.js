515d493551ba0e31aa3dba8c48153f0c58ad31c6
Nat <fthbby@gmail.com>
2022-03-05 12:49:43 -0800

    login & register working

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,7 +4,7 @@ const db = require('../models');
 
 
 const register = async (req, res) => {
-    const { userName, password, screenName } = req.body
+    const { userName, password, screenName} = req.body
     if(password.length < 6){
         return res.status(400).json({message: "Password must be longer than 6 characters"})
     }
@@ -29,7 +29,42 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
+    try {
+        const foundUser = await db.User.findOne({userName: req.body.userName}).select("+password")
 
+        if (!foundUser) {
+            return res
+                .status(400)
+                .json({status: 400, message: "cannot find user in database"})
+        }
+
+        const isMatch = await bcrypt.compare(req.body.password, foundUser.password)
+        // check if the password match
+        if(isMatch) {
+            // jwt.sign(payload, secret key for signing, config object)
+            // signature ensure that the token hasn't been altered, and we sign off on that with our secret key
+            const token = jwt.sign({_id: foundUser._id}, "hailsatan", {
+                expiresIn: "3h"
+            })
+
+            return res.status(200).json({
+                status: 200,
+                message: "GREAT SUCCESS",
+                token,
+            })
+        } else {
+            //the password provided doesn't match the password in the database
+            return res.status(400).json({
+                status: 400,
+                message: "password is wrong, Homie :((("
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: "Site broke or something. oop. Try again."
+        })
+    }
 }
 
 module.exports = {
