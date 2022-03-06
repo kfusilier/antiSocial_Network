e781c027fcdd96cff716fc8cf2c1fb949ed866be
kfusilier <kristinfusilier@gmail.com>
2022-03-06 07:12:30 -0700

    avatar component done

--- client/src/components/UserProfile/index.jsx
+++ client/src/components/UserProfile/index.jsx
@@ -1,9 +1,30 @@
 import style from './userProfile.module.css';
+import Avatar from '../Avatar';
 
 const UserProfile = () => {
 	return (
 		<div className={style.userProfile}>
-			<p>Profile</p>
+			<p>User Profile</p>
+			<div className={style.avatarPlacement}>
+				<Avatar />
+			</div>
+			<div className={style.edit}>
+				<p>edit user name</p>
+			</div>
+			<div className={style.toggle}>
+				<p>privacy toggle?</p>
+			</div>
+
+
+			<form className={style.form}>
+				<label className={style.label}>
+				Create Post</label>
+				<input className={style.input}></input>
+				<input
+					className={style.button}
+					type='submit'
+					value='submit'
+					</form>
 		</div>
 	);
 };
