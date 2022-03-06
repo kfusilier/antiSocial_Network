2c1c23144d7e9aced30288efae0ea685eb9b035f
kfusilier <kristinfusilier@gmail.com>
2022-03-06 14:09:13 -0700

    Converted NavBar 1 and Navbar2 into Navbar 2, all links showing in Nav now

--- client/src/components/navbar2/index.jsx
+++ client/src/components/navbar2/index.jsx
@@ -9,7 +9,23 @@ export default function NavBar2() {
                 <Link to="/">Home</Link>
                 <Link to="/posts">Posts</Link> */}
 				{/* NavLink is helpful when differentiating between nav links and regular links. It also automatically assigns an "isActive" class that makes it easier to style! */}
-
+				<NavLink
+					to='/SignupPage'
+					style={({ isActive }) => ({
+						color: isActive ? '#37ecba' : 'white',
+						textDecoration: 'none',
+						padding: '10px',
+					})}>
+					SignUp
+				</NavLink>
+				<NavLink
+					to='/LoginPage'
+					style={({ isActive }) => ({
+						color: isActive ? '#37ecba' : 'white',
+						textDecoration: 'none',
+					})}>
+					Login
+				</NavLink>
 				<NavLink
 					to='/BrowsePage'
 					style={({ isActive }) => ({
