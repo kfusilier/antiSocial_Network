3cfcfcf2396a20eba74f34e2c521fe6f7fa609d5
kfusilier <kristinfusilier@gmail.com>
2022-03-07 18:01:40 -0700

    Finished styling and linking Browsepage JSX to data

--- client/src/components/CommentShow/index.jsx
+++ client/src/components/CommentShow/index.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import antiSocialAppTo from '../../api/axios.config';
 import * as postService from '../../api/post.service';
 import { Route, NavLink } from 'react-router-dom';
-// import style from './commentShow.module.css';
 
 const CommentsList = () => {
 	const [posts, setPosts] = useState([]);
@@ -20,29 +19,41 @@ const CommentsList = () => {
 
 	return (
 		<div>
-			{posts.map((post) => {
-				return (
-					<div>
-						<p>
-							Post: {post.text} <br />
-							By User: {post._id}
-							{/* Created at: {post.createdAt} */}
-							<ul>
-								{post.comments.map((sub) => (
-									<li>{sub.content}</li>
-								))}
-								<NavLink to={`/posts/${post._id}`}>
-									<button
-										type='button'
-										className={style.button}>
-										View Post
-									</button>
-								</NavLink>
-							</ul>
-						</p>
-					</div>
-				);
-			})}
+			<div id={style.title}>
+				<p>Browse Posts</p>
+			</div>
+			<div className={style.center}>
+				<div className={style.container}>
+					{posts.map((post) => {
+						return (
+							<div className={style.post}>
+								<p className={style.text}>
+									<br />
+									Post: {post.text} <br />
+									Author ID: {post._id}
+									{/* Created at: {post.createdAt} */}
+									<ul>
+										{post.comments.map((sub) => (
+											<li>{sub.content}</li>
+										))}
+										<NavLink
+											to={`/posts/${post._id}`}>
+											<br />
+											<button
+												type='button'
+												className={
+													style.button
+												}>
+												View Post
+											</button>
+										</NavLink>
+									</ul>
+								</p>
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 };
