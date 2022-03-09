04d5fa18b7a90d6cbc4f2b6ca09f0e3c80688d33
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-03-08 16:51:55 -0800

    heroku api fix

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // switch URL depending on whether ur local or pushing to heroku
 // const backendAPI = "http://localhost:4000/api";
-const backendAPI = "https://antisocialnetwork.herokuapp.com//api";
+const backendAPI = "https://antisocialnetwork.herokuapp.com/api";
 	
 const user = JSON.parse(localStorage.getItem('user'))
 // standard instance naming convention can be api, client or apiClient
