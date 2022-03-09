c502695be5cf08d6144e4444680191d35949b6c6
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-03-08 16:37:01 -0800

    swithcing config to heorku api

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // switch URL depending on whether ur local or pushing to heroku
-const backendAPI = "http://localhost:4000/api";
-// const backendAPI = "https://XXXXXXXXXXX.herokuapp.com/api";
+// const backendAPI = "http://localhost:4000/api";
+const backendAPI = "https://antisocialnetwork.herokuapp.com//api";
 	
 const user = JSON.parse(localStorage.getItem('user'))
 // standard instance naming convention can be api, client or apiClient
