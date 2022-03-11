342674b89f555f50c5a3d90effa83e1607d213b8
kfusilier <kristinfusilier@gmail.com>
2022-03-11 13:17:03 -0700

    build

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -1,18 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 // switch URL depending on whether ur local or pushing to heroku
-const backendAPI = "http://localhost:4000/api";
-// const backendAPI = "https://antisocialnetwork.herokuapp.com/api";
-	
-const user = JSON.parse(localStorage.getItem('user'))
+// const backendAPI = "http://localhost:4000/api";
+const backendAPI = 'https://antisocialnetwork.herokuapp.com/api';
+
+const user = JSON.parse(localStorage.getItem('user'));
 // standard instance naming convention can be api, client or apiClient
 const antiSocialAppTo = axios.create({
 	baseURL: `${backendAPI}`,
 	headers: {
-	"content-type": "application/json",
-	authorization: `Bearer ${user}`
+		'content-type': 'application/json',
+		authorization: `Bearer ${user}`,
 	},
-
 });
 
 export default antiSocialAppTo;
