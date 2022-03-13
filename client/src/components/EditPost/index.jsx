4dca423fc4e14311f7ce6a72c9dc4118f7133d04
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2022-03-12 16:01:21 -0800

    FINALY FULL CRUD on POSTS

--- client/src/components/EditPost/index.jsx
+++ client/src/components/EditPost/index.jsx
@@ -1,4 +1,4 @@
-import style from './editPost.module.css';
+import style from "./editPost.module.css";
 import { useEffect, useState } from "react";
 import antiSocialAppTo from "../../api/axios.config";
 import * as postService from "../../api/post.service";
@@ -7,70 +7,71 @@ import { useParams } from "react-router-dom";
 
 
 const EditPost = (props) => {
-	const [posts, setPosts] = useState([]);
-	let { id } = useParams();
-  
-	const fetchPosts = async () => {
-	  await postService.get(id).then((res) => {
-		//   console.log(res.data.data);
-		setPosts(res.data.data);
-	  });
-	};
-  
-	const deleteBtn = async () => {
-	  await postService.destroy(id).then(() => {
-		document.location = "/BrowsePage";
-	  });
-	};
+  const [posts, setPosts] = useState([]);
+  const [text, setText] = useState(props.text);
+  let { id } = useParams();
 
+  const fetchPosts = async () => {
+    await postService.get(id).then((res) => {
+      //   console.log(res.data.data);
+      setPosts(res.data.data);
+    });
+  };
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		// let commentId = ('');
-		let res = await postService.updateComment(id).then(() => {
-		//   setNewComment("");
-		});
-		console.log(res);
-		if (!res === 201) {
-		  alert(`bad: ${res.status}`);
-		}
-	  };
+  const deleteBtn = async () => {
+    await postService.destroy(id).then(() => {
+      document.location = "/BrowsePage";
+    });
+  };
 
+  const handleSubmit = async () => {
+    const updatePost = { text, user: `${props.user}` };
+    let res = await postService.update(props.postId, updatePost).then(() => {
+      setText("");
+      console.log(updatePost);
+      props.refreshPosts();
+	  document.location = "/BrowsePage";
+    });
+    if (!res === 201) {
+      alert(`bad: ${res.status}`);
+    }
+  };
 
-	useEffect(() => {
-	  fetchPosts();
-	}, []);
-  
-	return (
-	  <div>
-		<div id={style.title}>
-		  <p>This is the edit post page!</p>
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-		  <br/>
-          Post: <h3>{posts.text} </h3>
-		  <form onSubmit={handleSubmit}>
+  return (
+    <div>
+      <div id={style.title}>
+        <p>This is the edit post page!</p>
+        <br />
+        Post: <h3>{props.text} </h3>
+        <form>
           <label>
             Edit your post:
-            <input
+            <textarea
               type="text"
-              value={posts.text}
-            //   onChange={(e) => setComments(e.target.value)}
+              name="text"
+              placeholder="body"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
             />
           </label>
 
-          <input type="submit" />
+          <input
+            className={style.button}
+            type="submit"
+            value="Save"
+            onClick={handleSubmit}
+          ></input>
         </form>
-		<button className={style.button} onClick={deleteBtn}>
-              Deletes
-            </button>
-
-		</div>
-	  
-		
-	
-	  </div>
-	);
-  };
-  
+        <button className={style.button} onClick={deleteBtn}>
+          Deletes
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default EditPost;
--- client/src/pages/EditPostPage/index.jsx
+++ client/src/pages/EditPostPage/index.jsx
@@ -1,11 +1,32 @@
 import EditPost from '../../components/EditPost';
 import NavBar2 from '../../components/navbar2';
+import { useState, useEffect } from 'react';
+import * as postService from "../../api/post.service"
+import {useParams} from "react-router-dom"
 
 const EditPostPage = () => {
+	const [post, setPost] = useState([]);
+	const {id} = useParams();
+
+	const fetchPosts = async () => {
+		await postService.get(id).then((res) => {
+			console.log(res.data.data);
+			setPost(res.data.data)
+		});
+	};
+
+	useEffect(() => {
+		fetchPosts();
+	}, [])
+
 	return (
 		<div>
 			<NavBar2 />
-			<EditPost />
+			<EditPost 
+			text={post.text}
+			postId= {post._id}
+			// refreshPosts={fetchPosts}
+			/>
 		</div>
 	);
 };
